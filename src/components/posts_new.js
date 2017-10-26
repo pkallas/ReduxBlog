1bcd2ca636a361b39815e702b85b7983ee146eca
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-10-26 16:45:48 -0700

    Added link and route for posts/new

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        🇮🇷🇯🇵🇯🇪🇮🇪🇲🇩🇲🇪🇭🇰🇬🇺🇳🇪
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
